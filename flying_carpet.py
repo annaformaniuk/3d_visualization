8a221deb7b0af4f6c7e61502936d99701985fa72
annaformaniuk <a_form03@uni-muenster.de>
2019-08-10 23:33:18 +0200

    added a colorbar and a bit of stylization

--- flying_carpet.py
+++ flying_carpet.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
+"""
+To calculate the pixel location of a geospatial coordinate
+LoaelevationArray the WorldFile info
+"""
 def get_coordinates(file, dx, dy):
     px, rx, _, py, _, ry = file.GetGeoTransform()
     x = dx/rx + px
@@ -12,23 +16,25 @@ def get_coordinates(file, dx, dy):
 
 filepath = r"F:\Dokumente\Uni_Msc\2019_SS\PIGIS\additional_assignment\GermanyDGM1\rasters\smallest.tif"
 file = gdal.Open(filepath)
-ds = gdal_array.LoadFile(filepath)
-print(ds.shape)
+elevationArray = gdal_array.LoadFile(filepath)
+print(elevationArray.shape)
 
 axis1 = []
 axis2 = []
 
-for i in range(ds.shape[0]):
+for i in range(elevationArray.shape[0]):
     axis1.append(get_coordinates(file, i, 0)[0])
 
-for j in range(ds.shape[1]):
+for j in range(elevationArray.shape[1]):
     axis2.append(get_coordinates(file, 0, j)[1])
 
-print(axis1)
-print(axis2)
 x, y = np.meshgrid(axis2, axis1, sparse=True)
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot_surface(x, y, ds, rstride=1, cstride=1, cmap=cm.jet, linewidth=1, antialiased=True)
+flying_carpet = ax.plot_surface(x, y, elevationArray, rstride=1, cstride=1, cmap=cm.gist_earth, linewidth=1, antialiased=True)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Elevation')
+fig.colorbar(flying_carpet, shrink=0.5)
 plt.show()
